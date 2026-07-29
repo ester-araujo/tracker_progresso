3dbbe72b53f2900489e83debc0d33818e892592e
ester-araujo <aester08@gmail.com>
2026-07-28 22:55:39 -0300

    feat: adiciona painel de estatísticas com plotly e pandas

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import streamlit as st
-from database import get_all_projects, create_table, add_project
+import pandas as pd
+import plotly.express as px
+from database import(
+     get_all_projects,
+     create_table, 
+     add_project,
+     update_project, 
+     delete_project
+)
 
 #Inicia o DB
 create_table()
 
 #Configura pagina
-st.set_page_config(page_title="Tracker de Estudos", page_icon=":stars", layout="wide")
+st.set_page_config(page_title="Tracker de Estudos", page_icon=":chart_with_upwards_trend:", layout="wide")
 
 # Injetando CSS 
 st.markdown("""
@@ -101,19 +109,19 @@ st.sidebar.markdown("---")
 #Menu de Seleção 
 menu = st.sidebar.radio(
     "Selecione uma opção:",
-    ["Ver Projetos", "Adicionar Novo Projeto", "Estatisticas"]
+    ["Ver Projetos", "Adicionar Novo Projeto", "Estatísticas"]
 )
 
 
-#Titulo Principal 
+# ------ABA:Ver Projetos -------
 
 if menu == "Ver Projetos": 
     st.title("Meu Tracker de Estudos")
     st.subheader("Acompanhe seu progresso de estudos, cursos e certificações!")
 
-    st.write("Este é um aplicativo simples para acompanhar projetos pessoais.")
+    st.write("*Este é um aplicativo simples para acompanhar projetos pessoais.")
 
-    #Buscando a lista de projetos no db
+    #Recupera a lista de projetos no db
     projetos = get_all_projects()
 
     if not projetos:
@@ -139,7 +147,43 @@ if menu == "Ver Projetos":
                 with col2:
                     st.metric(label="Progresso", value=f"{passo_atual}/{total_passos}")
                     st.progress(porcentagem)
+    
+# Área de edição e exclusão de projetos
+                with st.expander("Gerenciar Projetos"):
+                    col_input, col_note = st.columns([1,2])
 
+                    with col_input:
+                        novo_passo = st.number_input(
+                            "Passo Atual", 
+                            min_value=0,
+                            max_value=total_passos,
+                            value=passo_atual, 
+                            key=f"passo_{id_proj}" 
+                        )
+                    
+                    with col_note:
+                        nova_anotacao = st.text_input(
+                            "Nova Anotação", 
+                            value=ultima_anotacao if ultima_anotacao else "", 
+                            key=f"note_{id_proj}"
+                        )
+                    
+                    bnt_salvar, bnt_excluir = st.columns([1,1])
+
+                    with bnt_salvar:
+                        if st.button("Salvar Projeto", key=f"bnt_save_{id_proj}"):
+                            update_project(id_proj, passo_atual, ultima_anotacao)
+                            st.success("Progresso atualizado com sucesso!")
+                            st.rerun()
+
+                    with bnt_excluir:
+                        if st.button("Excluir Projeto", key=f"bnt_del_{id_proj}"):
+                            delete_project(id_proj)
+                            st.warning("Projeto Excluído")
+                            st.rerun()
+
+                        
+# ------ABA:Adicionar Novo Projeto -------
 elif menu == "Adicionar Novo Projeto":
     st.title("Adicionar Novo Projeto")
     st.write("Preencha os campos abaixo pra adicionar um novo projeto")
@@ -177,6 +221,77 @@ elif menu == "Adicionar Novo Projeto":
             except Exception as e:
                 st.error(f"Erro ao adicionar projeto: {e}")
 
+# ------ABA:Estatísticas -------
 elif menu == "Estatísticas":
-    st.title("Visão Geral")
-    st.write("Aqui vai ter graficos de estatiticas") 
+    st.title("Painel de Desempenho")
+    st.write("Análise em tempo real seu progresso.")
+
+    projetos = get_all_projects()
+
+    if not projetos:
+        st.info("Nenhum dado disponível. Adicione porjetos para começar a visualizar suas estastísticas!")
+    else:
+        #Transforma os dados em um DataFrame Pandas
+        df = pd.DataFrame(
+            projetos,
+            columns=["id","titulo","categoria","total_passos", "passo_atual", "ultima_anotacao", "data_atualizacao"]
+        )
+
+        #Calcula a porcentagem de progesso de cada projeto 
+        df["progresso_pct"] = (df["passo_atual"]/ df["total_passos"])*100
+
+        #Metricas de Topo(KPIs)
+        total_projetos = len(df)
+        concluidos = len(df[df["passo_atual"]==df["total_passos"]])
+        total_modulos = df["total_passos"].sum()
+        modulos_completos = df["passo_atual"].sum()
+
+        col_kpi1, col_kpi2, col_kpi3, col_kpi4 = st.columns(4)
+        col_kpi1.metric("Total de Projetos", total_projetos)
+        col_kpi2.metric("Projetos Concluídos", concluidos)
+        col_kpi3.metric("Total de Módulos", total_modulos)
+        col_kpi4.metric("Módulos Concluídos", modulos_completos)
+
+        st.markdown("---")
+
+        #---GRAFICOS INTERATIVOS COM PLOTLY---
+        col_graf1, col_graf2 = st.columns(2)
+
+        with col_graf1:
+            #Grafico de barras
+            fig_barras = px.bar(
+                df,
+                x="progresso_pct",
+                y="titulo",
+                orientation="h",
+                title="Progresso por Projeto (%)",
+                labels={"progresso_pct": "PRogresso (%)","titulo":"Projeto"},
+                color_discrete_sequence=["#FF4191"]
+            )
+
+            fig_barras.update_layout(
+                paper_bgcolor="rgba(0,0,0,0)",
+                plot_bgcolor="rgba(0,0,0)",
+                font_color="#F4F1EA",
+                xaxis=dict(range=[0, 100], gridcolor="#161224")
+            )
+
+            st.plotly_chart(fig_barras, use_container_width=True)
+
+            with col_graf2:
+                #Grafico de Rosca
+                fig_rosca =px.pie(
+                    df, 
+                    names="categoria",
+                    title="Distribuição por Categoria",
+                    hole=0.5,
+                    color_discrete_sequence=["#A3FFD6", "#A370F7", "#FF4191", "#F4F1EA"]
+                )
+
+                fig_rosca.update_layout(
+                    paper_bgcolor="rgba(0,0,0,0)",
+                    plot_bgcolor="rgba(0,0,0,0)", 
+                    font_color="#F4F1EA"
+                )
+
+                st.plotly_chart(fig_rosca, use_container_width=True)
